85e51a1612332536e786f5f9cf5697bc429d2364
George Holborn <georgeholborn5@gmail.com>
2020-05-06 15:57:46 +0100

    created functional station class

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,7 +2,15 @@ require "station.rb"
 
 describe Station do
   it "should create a new instance of itself" do
-    oldstreet = Station.new
+    oldstreet = Station.new("old_street", "zone_1")
     expect(oldstreet).to be_instance_of Station
   end
+  it 'should initialize a name instance variable' do
+    oldstreet = Station.new("old_street", "zone_1")
+    expect(oldstreet.name).to eq("old_street")
+  end
+    it 'should initialize a zone instance variable' do
+      oldstreet = Station.new("old_street", "zone_1")
+      expect(oldstreet.zone).to eq("zone_1")
+    end
 end
