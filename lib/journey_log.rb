f4eeb174db6e2b40a146a545db7a0b0429a4a02b
George Holborn <georgeholborn5@gmail.com>
2020-05-06 15:14:55 +0100

    created JourneyLOg class and Testss

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,2 +1,10 @@
 class JourneyLog
+attr_reader :history
+  def initialize
+    @history = []
+  end
+
+  def update(station)
+    @history << station
+  end
 end
---
+++ spec/journey_log_spec.rb
@@ -0,0 +1,20 @@
+require 'journey_log.rb'
+
+describe JourneyLog do
+  describe '#journey log' do
+    let(:old_street) { double :station }
+    it 'creates a new instance of itself' do
+      log = JourneyLog.new
+      expect(log).to be_instance_of JourneyLog
+      end
+    it 'creates empty array' do
+      log = JourneyLog.new
+      expect(log.history).to eq([])
+      end
+    it 'creates empty array' do
+      log = JourneyLog.new
+      log.update(old_street)
+      expect(log.history).to eq([old_street])
+    end
+  end
+end
